0e2e12091bd6e36e37943eaca545949f2e7d00ff
Mohine <166409071+MohineAlam@users.noreply.github.com>
2025-09-30 15:15:25 +0100

    Update and rename test_pytorch_model.py to test_default_pytorch_model.py

---
+++ test_default_pytorch_model.py
@@ -0,0 +1,10 @@
+
+####---- import libraries ----####
+from lucam import Lucam
+from torchvision import transforms, models
+import cv2
+import torch
+import numpy as np
+import os
+import argparse
+import sys
